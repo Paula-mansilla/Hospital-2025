ef4a067e84667d49d4c728b7dd732571f07c0464
sofiia <snsandoval@scl.edu.gt>
2025-06-13 18:50:29 -0600

    Actualización botones

--- Paneles/MenuLateralPanel.java
+++ Paneles/MenuLateralPanel.java
@@ -10,6 +10,10 @@ public class MenuLateralPanel extends JPanel {
     // Botones agregados
     private JButton btnPacientes;
     private JButton btnHistorial;
+    private JButton btnHabitaciones;
+    private JButton btnDoctores;
+    private JButton btnCirujanos;
+    private JButton btnServiciosLimpieza;
     
     public MenuLateralPanel() {
         this.setPreferredSize(new Dimension(400, 1000));
@@ -18,15 +22,31 @@ public class MenuLateralPanel extends JPanel {
 
         // --- AGREGADO: Creación de botones ---
         btnPacientes = new JButton("Pacientes");
-        btnPacientes.setBounds(50, 50, 120, 40); // (x, y, ancho, alto)
+        btnPacientes.setBounds(20, 50, 150, 40); // (x, y, ancho, alto)
         
         btnHistorial = new JButton("Historial");
-        btnHistorial.setBounds(50, 120, 120, 40); // Posición debajo del primero
+        btnHistorial.setBounds(180, 50, 150, 40); // Posición debajo del primero
+
+        btnHabitaciones = new JButton("Habitaciones");
+        btnHabitaciones.setBounds(20, 110, 150, 40);
+
+        btnDoctores = new JButton("Doctores");
+        btnDoctores.setBounds(180, 110, 150, 40);
+
+        btnCirujanos = new JButton("Cirujanos");
+        btnCirujanos.setBounds(20, 170, 150, 40);
+
+        btnServiciosLimpieza = new JButton("Servicios de Limpieza");
+        btnServiciosLimpieza.setBounds(180, 170, 150, 40);
 
         // Agregar botones al panel
         this.setLayout(null); // Layout nulo para usar setBounds
         this.add(btnPacientes);
         this.add(btnHistorial);
+        this.add(btnHabitaciones);
+        this.add(btnDoctores);
+        this.add(btnCirujanos);
+        this.add(btnServiciosLimpieza);
         // --- FIN AGREGADO ---
     }
     
